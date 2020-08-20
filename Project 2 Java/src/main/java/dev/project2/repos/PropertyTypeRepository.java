2288816a6f2b13d4b604204d9fbd096803d0e839
jaynyc2020 <67928738+jaynyc2020@users.noreply.github.com>
2020-08-20 12:48:41 -0400

    Adding more functionality to Controllers and commented out JSON

--- Project 2 Java/src/main/java/dev/project2/repos/PropertyTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.project2.repos;
-
-import dev.project2.entities.PropertyType;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PropertyTypeRepository extends CrudRepository<PropertyType, Integer>
-{
-}
--- frontend/src/app/models/property-type.ts
+++
@@ -1,8 +0,0 @@
-export class PropertyType {
-    
-    propertyTypeId:number[];
-
-    constructor(price:number){
-
-    }
-}
