d00803864f3bf0934b7dbdcd4acc062d7a8222a6
NoahWhite1 <41762591+NoahWhite1@users.noreply.github.com>
2020-08-22 14:09:14 -0400

    adding E2E tests to project 2

---
+++ Project 2 Java/src/test/java/dev/project2/steps/AgentSteps.java
@@ -0,0 +1,116 @@
+package dev.project2.steps;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class AgentSteps {
+
+	@Given("^The agent is on the login page$")
+	public void the_agent_is_on_the_login_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The agent inputs a username$")
+	public void the_agent_inputs_a_username() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The agent inputs a password$")
+	public void the_agent_inputs_a_password() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The agent clicks the login button$")
+	public void the_agent_clicks_the_login_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^The agent should be taken to the agent portal$")
+	public void the_agent_should_be_taken_to_the_agent_portal() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^The agent is on the login portal$")
+	public void the_agent_is_on_the_login_portal() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The agent inputs the username and password$")
+	public void the_agent_inputs_the_username_and_password() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The agent clicks the submit button$")
+	public void the_agent_clicks_the_submit_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^There is an alert displaying credential error$")
+	public void there_is_an_alert_displaying_credential_error() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^The agent is on the agent portal page$")
+	public void the_agent_is_on_the_agent_portal_page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^The agent should see all their managed properties$")
+	public void the_agent_should_see_all_their_managed_properties() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^The agent is on the agent portal$")
+	public void the_agent_is_on_the_agent_portal() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The agent can modify mandatory fields$")
+	public void the_agent_can_modify_mandatory_fields() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The agent clicks on the update property button$")
+	public void the_agent_clicks_on_the_update_property_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Alert box to let agent know if a property was updated or not$")
+	public void alert_box_to_let_agent_know_if_a_property_was_updated_or_not() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^The agent clicks on a property card$")
+	public void the_agent_clicks_on_a_property_card() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^The agent is redirected to a page with the correct property credentials$")
+	public void the_agent_is_redirected_to_a_page_with_the_correct_property_credentials() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
