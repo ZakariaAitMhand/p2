4016d9de2754e6388a20bd9af762ed159457681e
Your Name <zak.aitmhand@gmail.com>
2020-08-22 12:32:05 -0400

    css problems solved + login logout + delete properties click

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AgentService } from '../../services/agent/agent.service';
-import { Router } from '@angular/router';
-import {PropertyService} from '../../services/property/property.service';
-import {HomePageComponent} from '../home-page/home-page.component';
 
 @Component({
   selector: 'app-menu',
@@ -11,57 +8,43 @@ import {HomePageComponent} from '../home-page/home-page.component';
 })
 export class MenuComponent implements OnInit {
 
-  private homePage:HomePageComponent;
-  constructor(public agentService:AgentService, private router:Router, private propertyService:PropertyService) { }
+  constructor(public agentService:AgentService) { }
 
   isLogedIn:boolean = (this.agentService.loggedInAgent===undefined)?false:true;
   ngOnInit(): void {
     // console.log("from menu "+this.agentService.loggedInAgent);
+    if(localStorage.getItem('agent')){
+      this.agentService.loggedInAgent = JSON.parse(localStorage.getItem('agent'));
+    }
   }
 
   agentsBtnClick():void{
-    this.router.navigate(['/displayagents']);
     // console.log("loggedInAgent " + this.agentService.loggedInAgent);
   }
 
-  backTransparent:boolean=false;
-  LoginContainer:boolean=false;
-  createPropertyContainer:boolean=false;
-
   loginPopup(){
-    this.backTransparent=true;
-    this.LoginContainer=true;
+    this.agentService.LoginContainerBackTransparent=true;
+    this.agentService.LoginContainer=true;
   }
 
   logoutClick(){
     this.agentService.loggedInAgent = undefined;
+    localStorage.setItem('agent', '');
   }
 
 
   loginPopupClose(){
-    this.backTransparent=false;
-    this.LoginContainer=false;
+    this.agentService.LoginContainerBackTransparent=false;
+    this.agentService.LoginContainer=false;
   }
 
   createPropertyClose(){
-    this.backTransparent=false;
-    this.createPropertyContainer=false;
+    this.agentService.createPropertyContainerBlackTransparent=false;
+    this.agentService.createPropertyContainer=false;
   }
 
   createProperty(){
-    this.backTransparent=true;
-    this.createPropertyContainer=true;
-  }
-
-  async highToLow(){
-    //this.homePage.properties = await this.propertyService.getAllPropertiesHighToLow();
-    console.log("getting all properties high to low");
-
-  }
-
-  async lowToHigh(){
-    //this.homePage.properties = await this.propertyService.getAllPropertiesLowToHigh();
-    console.log("getting all properties low to high");
-
+    this.agentService.createPropertyContainerBlackTransparent=true;
+    this.agentService.createPropertyContainer=true;
   }
 }
--- frontend/src/app/components/property-card/property-card.component.ts
+++ frontend/src/app/components/property-card/property-card.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Property } from 'src/app/models/property';
-import { stringify } from 'querystring';
-import { PropertyService } from 'src/app/services/property/property.service';
-import { RouterLink, Router } from '@angular/router';
+import { AgentService } from '../../services/agent/agent.service';
+
 
 @Component({
   selector: 'app-property-card',
@@ -11,39 +10,27 @@ import { RouterLink, Router } from '@angular/router';
 })
 export class PropertyCardComponent implements OnInit {
 
-  
   @Input("property") property:Property;
-   cardImage:string[] = [];
-
   //@Input() newProperty:Property;
 
-  propertyHomeImage:string;
-
-  constructor(private propServ:PropertyService, private router:Router) { }
+  constructor(public agentService:AgentService) { }
 
   propertyCard: Property;
   ngOnInit(): void {
     // console.log(this.property);
     this.propertyCard = this.property;
-    this.getPropertyCardHomeImage();
   }
 
-  propertyClick(){
+  propertyClick(id){
     // alert("property Click")
-    this.propServ.propertyImages = this.cardImage;
-    this.router.navigate(['/displayproperty'])
   }
   
   deletePropertyClick(id:number){
     // Call for delete endpoint
-    console.log(id);
+    alert(id);
   }
 //   property: any/*Property*/ = 
 //     {location: this.newProperty.location, price:this.newProperty.price, squareFeet: this.newProperty.squareFeet};
-
-getPropertyCardHomeImage(){
-  this.cardImage = this.propertyCard.image_url.split(',');
-}
 
 
 }
